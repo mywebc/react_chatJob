2544b8da937c05c00a4040df44a4e6abe3d222ac
陈晓拉尼 <2524729719@qq.com>
2019-01-16 17:02:57 +0800

    数据库模型建立

--- server/model.js
+++ server/model.js
@@ -1,8 +1,44 @@
 const mongoose = require('mongoose')
 
 // 连接数据库，并且创建react-job这个集合
-const db_URL = 'mongodb://127.0.0.1:27017/react_job'
+const db_URL = 'mongodb://127.0.0.1:27017/react_job_chat'
 mongoose.connect(db_URL)
+
+// 定义模型
+const models = {
+    user: {
+        'user': { type: String, require: true },
+        'pwd': { type: String, require: true },
+        'type': { type: String, require: true },
+        'avatar': { type: String },
+        'desc': { type: String },
+        'title': { type: String },
+        'company': { type: String },
+        'money': { type: String }
+    },
+    chat: {}
+}
+
+// 批量生成模型
+for(let m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+    getModel: function(name) {
+        return mongoose.model(name)
+    }
+}
+
+
+
+
+
+
+
+
+
+
 mongoose.connection.on('connected', function() {
     console.log('mongo connect success')
 })
--- server/user.js
+++ server/user.js
@@ -1,6 +1,9 @@
 const express = require("express")
 // 使用Router中间件
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
+
 
 Router.get('/info', function(req, res) {
     res.json({code: 0})
