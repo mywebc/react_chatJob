22d42e0496668b7af7a8b4ec57b89d8c8c1a89bc
陈晓拉尼 <2524729719@qq.com>
2019-01-17 23:41:24 +0800

    完成登录注册

--- server/user.js
+++ server/user.js
@@ -5,6 +5,15 @@ const model = require('./model')
 const User = model.getModel('user')
 const utils = require('utility')
 
+Router.get('/list', function(req, res) {
+    User.remove({}, function(err, doc) {})
+    User.find({}, function(err, doc){
+        if(doc) {
+            return res.json(doc)
+        }
+    })
+})
+// 注册
 Router.post('/register', function(req, res) {
     console.log(req.body) 
     const { user, pwd, type } = req.body
@@ -19,6 +28,25 @@ Router.post('/register', function(req, res) {
             }
             return res.json({code: 0})
         })
+    })
+})
+// 登录
+Router.post('/login', function(req, res) {
+    const { user, pwd } = req.body
+    User.findOne({user}, function(err, doc) {
+        if(doc) {
+            console.log(doc)
+            const pwdServer = doc.pwd
+            const pwdClient = utils.md5(pwd)
+            if (pwdServer !== pwdClient) {
+                return res.json({code: 1, msg: '密码错误！'})
+            } else {
+                return res.json({code: 0, data: doc})
+            }
+
+        } else {
+            return res.json({code: 1, msg: '该用户不存在！'})
+        }
     })
 })
 Router.get('/info', function(req, res) {
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -4,6 +4,7 @@ import { getRedirectPath } from '../util'
 // 创建action对象（redux-thunk能让action为函数）交给reducer处理，reducer处理后并且返回新的state
 const ERROR_MSG = 'error_msg'
 const REGISTER_SUCCESS = 'register_success'
+const LOGIN_SUCCESS = 'login_success'
 
 // state初始
 const initState = {
@@ -17,6 +18,8 @@ const initState = {
 // reducer
 export function user(state = initState, action) {
     switch(action.type) {
+        case LOGIN_SUCCESS:
+            return {...state, msg: '', isAuth: true, redirectTo: getRedirectPath(action.payload), ...action.payload}
         case REGISTER_SUCCESS:
             return {...state, msg: '', isAuth: true, redirectTo: getRedirectPath(action.payload), ...action.payload}
         case ERROR_MSG:
@@ -32,6 +35,9 @@ function registerSuccess(data) {
 
 function errMsg(msg) {
     return { msg, type: ERROR_MSG }
+}
+function loginSuccess(data) {
+    return { type: LOGIN_SUCCESS, payload: data }
 }
 
 export function register({user, pwd, repeatpwd, type}) {
@@ -55,4 +61,23 @@ export function register({user, pwd, repeatpwd, type}) {
         })  
     }
     
+}
+
+export function login({ user, pwd }) {
+    if (!user || !pwd) {
+        console.log(user, pwd)
+        return errMsg('用户名或密码必须输入')
+    }
+    // redux-thunk允许返回一个函数
+    return dispatch => {
+        axios.post('/user/login', { user, pwd })
+        .then((res) => {
+            if (res.status === 200 && res.data.code === 0) {
+                dispatch(loginSuccess(res.data.data))
+            } else {
+                // 返回后台的错误信息
+                dispatch(errMsg(res.data.msg))
+            }
+        })  
+    }
 }
