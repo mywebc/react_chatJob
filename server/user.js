6f5f312313aa2fb0e07af2f757bf3ba950103552
陈晓拉尼 <2524729719@qq.com>
2019-01-24 19:38:20 +0800

    使用redux获取聊天列表

--- server/user.js
+++ server/user.js
@@ -3,6 +3,7 @@ const express = require("express")
 const Router = express.Router()
 const model = require('./model')
 const User = model.getModel('user')
+const Chat = model.getModel('chat')
 const utils = require('utility')
 
 Router.get('/list', function(req, res) {
@@ -88,4 +89,23 @@ Router.post('/update', function(req, res) {
     })
 })
 
+
+
+Router.get('/getmsglist', function (req, res) {
+    const user = req.cookies.userid
+  
+    User.find({}, function (err, userdoc) {
+      if (!err) {
+        let users = {}
+        userdoc.forEach(v => {
+          users[v._id] = {name: v.user, avatar: v.avatar}
+        })
+        Chat.find({'$or': [{from: user}, {to: user}]}, function (err, doc) {
+          if (!err) {
+            return res.json({code: 0, msgs: doc, users: users})
+          }
+        })
+      }
+    })
+  })
 module.exports = Router
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import io from 'socket.io-client'
 import { List, InputItem } from 'antd-mobile'
+import { connect } from 'react-redux' 
+import { getMsgList } from '../../redux/chat.redux'
 
-const socket = io("ws://localhost:9090")
 // 监听全局
-socket.on('receiveMsg', function(data) {
-    console.log(data)
-})
+// socket.on('receiveMsg', function(data) {
+//     console.log(data)
+// })
+@connect(
+    state => state,
+    { getMsgList }
+)
 class Chat extends React.Component {
     constructor(props) {
         super(props)
@@ -15,12 +19,13 @@ class Chat extends React.Component {
         }
     }
     componentDidMount() {
+        this.props.getMsgList()
     }
     handleSubmit() {
-        // 向后台发送事件
-        socket.emit('sendMsg', {text: this.state.text})
-        // 清空
-        this.setState({text: ''})
+        // // 向后台发送事件
+        // socket.emit('sendMsg', {text: this.state.text})
+        // // 清空
+        // this.setState({text: ''})
     }
     render() {
         return (
